3937a9991a06886959ab04877fca775a3fb38972
Ahmad Saleh <ahmadrocks2011@hotmail.com>
2022-09-02 20:17:19 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 5;
 
+let multiply = num1 * num2;
+let random = parseInt(Math.random()*10);
+console.log(random);
+let mod = num3 % num4;
+let max = Math.max(1, 20, 7, 3, 15);
 // Write your code here
